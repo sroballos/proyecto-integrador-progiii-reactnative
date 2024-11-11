7d02d2fa15ba9330f9bfd9ab5104d00ca34a4fb9
CarolinaMartinezz <cbmartinez@udesa.edu.ar>
2024-11-11 16:18:10 -0300

    Comienzo del formulario de registración

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,12 +1,136 @@
-import { Component } from 'react'
-import { Text, View } from 'react-native-web'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+import { auth, db } from "../firebase/config";
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      userName: "",
+      password: "",
+      registered: false,
+      errorMSG: "",
+    };
+  }
+
+  handleSubmit = () => {
+    console.log("Email:", this.state.email);
+    console.log("UserName:", this.state.userName);
+    console.log("Password:", this.state.password);
+
+    auth
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then((response) => {
+       
+        if (response) {
+          db.collection("users")
+            .add({ 
+              email: this.state.email, 
+              userName: this.state.userName,
+              createdAt: Date.now(),
+            })
+
+            .then(this.props.navigation.navigate("Login")) 
+            .catch((err) => console.log(err));
+
+          this.setState({ registered: true, errorMSG: "" });
+        }
+
+      })
+
+      .catch((err) => this.setState({ errorMSG: err.message }));
+  };
+
   render() {
     return (
-      <View>
-        <Text>Register</Text>
+      <View style={styles.container}>
+        <Text>Hola, esto es el register</Text>
+
+        <TextInput
+          style={styles.input}
+          keyboardType="default"
+          placeholder="UserName"
+          onChangeText={(text) => this.setState({ userName: text })}
+          value={this.state.userName}
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="email-address"
+          placeholder="Email"
+          onChangeText={(text) => this.setState({ email: text })}
+          value={this.state.email}
+        />
+
+        <TextInput
+          style={styles.input}
+          keyboardType="default"
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({ password: text })}
+          value={this.state.password}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={this.handleSubmit}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+
+        {this.state.errorMSG && <Text>{this.state.errorMSG}</Text>}
+
+        <Text style={styles.accountText}>Already have an account?</Text>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("Login")}
+        >
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "#2196F3",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  accountText: {
+    marginTop: 20,
+    fontSize: 16,
+    color: "#555",
+  },
+  loginButtonText: {
+    fontSize: 16,
+    color: "#2196F3",
+    fontWeight: "bold",
+  },
+});
