fda887f9abcd9ca8764145cc18857da910be821a
sroballos <sroballos@udesa.edu.ar>
2024-11-11 15:36:02 -0300

    organización general

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeMenu from "./src/components/HomeMenu"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <HomeMenu />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/HomeMenu.js
@@ -0,0 +1,9 @@
+import { Text, View, TouchableOpacity, FlatList } from "react-native-web"
+
+export default class HomeMenu extends Component {
+  render() {
+    return (
+      <Text>Hola</Text>
+    )
+  }
+}
