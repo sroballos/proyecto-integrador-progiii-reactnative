a5a057233c9ac0917e6d98f2b24fe83b4be38d34
sroballos <sroballos@udesa.edu.ar>
2024-11-11 15:58:15 -0300

    firebase, post

---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+import { Text } from 'react-native-web'
+
+export default class Post extends Component {
+  render() {
+    return (
+      <Text>Post</Text>
+    )
+  }
+}
