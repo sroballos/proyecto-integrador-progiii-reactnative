1114174f75a609b8cbaa37ec4422fe048f2b59ad
sroballos <sroballos@udesa.edu.ar>
2024-11-11 16:06:20 -0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBixCOb4BD9opsyQf9wtUTw50xKYDvPtnQ",
+  authDomain: "prog3reactnative.firebaseapp.com",
+  projectId: "prog3reactnative",
+  storageBucket: "prog3reactnative.firebasestorage.app",
+  messagingSenderId: "184521400851",
+  appId: "1:184521400851:web:72e7d0bdcff7a05aac55e6"
+};
+
+const app = initializeApp(firebaseConfig);
